7f415326e83266bc31c88a64e5d4267b82d439c9
pengzihua2026-anyang <pengzihua2026@example.com>
2026-04-01 14:39:39 +0800

    Initial Commit

---
+++ binary/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  binary
+//
+//  Created by Zihua on 2026/4/1.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
